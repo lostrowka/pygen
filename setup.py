a7c378a1412309e21944368948d96b5c45bd7936
Lukasz Ostrowka <lostrowka@gmail.com>
2019-06-02 12:05:45 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pygen',
+    version='0.1',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='lostrowka',
+    author_email='lostrowka@gmail.com',
+    description='',
+    install_requires=['PySide2']
+)
